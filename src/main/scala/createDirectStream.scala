96ddde9eb723309c6319a884471ef7a6e9b9b9ce
phuccoi996@gmail.com <phuccoi996@gmail.com>
2020-08-17 16:28:37 +0900

    auto updates

--- src/main/scala/createDirectStream.scala
+++ src/main/scala/createDirectStream.scala
@@ -1,3 +1,10 @@
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+
+
 class createDirectStream {
 
 }
